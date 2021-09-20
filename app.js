03dca08e4ddbfff60391db4a649a7f2e40bb3116
Roshannair24 <70207465+Roshannair24@users.noreply.github.com>
2021-09-20 11:16:33 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var serverurl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+// var serverurl = "https://api.funtranslations.com/translate/minion.json";
